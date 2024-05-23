7042aab6328ec02d5091aa244816607e05fb4729
Lowly <87949654+L0w1y@users.noreply.github.com>
2024-05-23 17:44:55 +0700

    Big update, but dick is still small

--- app/page.tsx
+++ app/page.tsx
@@ -7,12 +7,16 @@ import { Button } from "@/components/ui/button"
 import { JSX, SVGProps } from "react"
 // import { url } from "inspector";
 import Cover from "/public/cover.png";
-import Placeholder from "/public/placeholder.svg";
+import Avatar1 from "@/public/artworks/wm7qcP9qG1Q.jpg";
+import Avatar2 from "@/public/artworks/LM7F0uMdEU8.jpg";
+import Avatar3 from "@/public/artworks/MLA3TcFokaM.jpg";
+import Avatar4 from "@/public/artworks/MLAPTcFokaM.jpg";
+// import Placeholder from "/public/placeholder.svg";
 
 export default function Home() {
   return (
     <>
-      <section className="w-full py-12 md:py-24 lg:py-32 xl:py-48 relative">
+      <section className="w-full py-12 md:py-24 lg:py-32 xl:py-48 center">
         <div className="absolute inset-0">
           <Image
             alt="Hero"
@@ -22,7 +26,6 @@ export default function Home() {
           />
           <div className="absolute inset-0 bg-gradient-to-l from-transparent from-1% to-white dark:to-black"/>
         </div>
-      
         <div className="container px-4 md:px-6 relative z-10">
           <div className="grid gap-6 lg:grid-cols-[1fr_400px] lg:gap-12 xl:grid-cols-[1fr_600px]">
             <div className="flex flex-col justify-center space-y-4">
@@ -52,58 +55,52 @@ export default function Home() {
               </p>
             </div>
             <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+
+              {/* Showcase Our Creativity */}
+
               <div className="rounded-lg bg-white p-4 shadow-sm dark:bg-gray-950">
-                <div className="mt-2 text-center">
                 <Image
                   alt="Avatar 1"
                   className="mx-auto aspect-square overflow-hidden rounded-lg object-cover"
                   height="300"
-                  src={Placeholder}
+                  src={Avatar1}
                   width="300"
                   />
-                  <h3 className="text-sm font-semibold">Avatar name</h3>
-                  <p className="text-xs text-gray-500 dark:text-gray-400">Sample pack</p>
-                </div>
               </div>
               <div className="rounded-lg bg-white p-4 shadow-sm dark:bg-gray-950">
                 <Image
                   alt="Avatar 2"
                   className="mx-auto aspect-square overflow-hidden rounded-lg object-cover"
                   height="300"
-                  src={Placeholder}
+                  src={Avatar2}
                   width="300"
                 />
-                <div className="mt-2 text-center">
-                  <h3 className="text-sm font-semibold">Avatar name</h3>
-                  <p className="text-xs text-gray-500 dark:text-gray-400">Sample pack</p>
-                </div>
               </div>
               <div className="rounded-lg bg-white p-4 shadow-sm dark:bg-gray-950">
                 <Image
                   alt="Avatar 3"
                   className="mx-auto aspect-square overflow-hidden rounded-lg object-cover"
                   height="300"
-                  src={Placeholder}
+                  src={Avatar3}
                   width="300"
                 />
-                <div className="mt-2 text-center">
-                  <h3 className="text-sm font-semibold">Avatar name</h3>
-                  <p className="text-xs text-gray-500 dark:text-gray-400">Sample pack</p>
-                </div>
               </div>
               <div className="rounded-lg bg-white p-4 shadow-sm dark:bg-gray-950">
                 <Image
                   alt="Avatar 4"
                   className="mx-auto aspect-square overflow-hidden rounded-lg object-cover"
                   height="300"
-                  src={Placeholder}
+                  src={Avatar4}
                   width="300"
                 />
-                <div className="mt-2 text-center">
+                {/* <div className="mt-2 text-center">
                   <h3 className="text-sm font-semibold">Avatar name</h3>
                   <p className="text-xs text-gray-500 dark:text-gray-400">Sample pack</p>
-                </div>
+                </div> */}
               </div>
+              
+              {/* End of Showcase Our Creativity */}
+
             </div>
           </div>
         </div>
@@ -113,48 +110,48 @@ export default function Home() {
           <div className="space-y-2 text-center">
             <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">Artwork pricing</h2>
             <p className="mx-auto max-w-[700px] text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed dark:text-gray-400">
-              Choose the plan that fits to your needs. Switch between &apos;Simple&apos; and &apos;Complicated&apos; to see the different
+              Choose the plan that fits to your needs. Switch between &apos;Textures&apos; and &apos;Avatars&apos; to see the different
               features.
             </p>
           </div>
           <div className="mx-auto mt-8 max-w-3xl">
-            <Tabs className="w-full" defaultValue="simple">
+            <Tabs className="w-full" defaultValue="textures">
               <TabsList className="grid w-[300px] grid-cols-2 rounded-full mx-auto">
-                <TabsTrigger className="rounded-full" value="simple">
-                  Simple
+                <TabsTrigger className="rounded-full" value="textures">
+                  Textures
                 </TabsTrigger>
-                <TabsTrigger className="rounded-full" value="complicated">
-                  Complicated
+                <TabsTrigger className="rounded-full" value="avatars">
+                  Avatars
                 </TabsTrigger>
               </TabsList>
-              <TabsContent value="simple">
+
+              <TabsContent value="textures">
                 <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-2">
+
+                {/* Simple cards group begin */}
+
                   <Card className="h-full">
                     <CardHeader>
-                      <CardTitle>Avatar name</CardTitle>
-                      <CardDescription>A simple avatar, with default materials, created for personal use.</CardDescription>
+                      <CardTitle>Simple texture</CardTitle>
+                      <CardDescription>A simple texture, with handwriten patterns, created for personal use only.</CardDescription>
                     </CardHeader>
                     <CardContent className="space-y-4">
                       <div className="space-y-1">
-                        <h3 className="text-4xl font-bold">from $0.99</h3>
-                        <p className="text-gray-500 dark:text-gray-400">estemated price.</p>
+                        <h3 className="text-4xl font-bold">from 18$</h3>
+                        <p className="text-gray-500 dark:text-gray-400">*price is not fixed</p>
                       </div>
                       <ul className="space-y-2 text-sm">
                         <li>
                           <CheckIcon className="mr-2 inline-block h-4 w-4 text-green-500" />
-                          Note
+                          4k Texture resolution 
                         </li>
                         <li>
                           <CheckIcon className="mr-2 inline-block h-4 w-4 text-green-500" />
-                          Note
+                          Any avatar base 
                         </li>
                         <li>
                           <CheckIcon className="mr-2 inline-block h-4 w-4 text-green-500" />
-                          Note
-                        </li>
-                        <li>
-                          <CheckIcon className="mr-2 inline-block h-4 w-4 text-green-500" />
-                          Note
+                          Minimalized emissions
                         </li>
                       </ul>
                     </CardContent>
@@ -164,30 +161,30 @@ export default function Home() {
                   </Card>
                   <Card className="h-full">
                     <CardHeader>
-                      <CardTitle>Avatar name</CardTitle>
-                      <CardDescription>A simple avatar, with default materials, created for personal use.</CardDescription>
+                      <CardTitle>Complicated texture</CardTitle>
+                      <CardDescription>A complicated texture set, with custom materials, created for personal use only.</CardDescription>
                     </CardHeader>
                     <CardContent className="space-y-4">
                       <div className="space-y-1">
-                        <h3 className="text-4xl font-bold">from $0.200</h3>
-                        <p className="text-gray-500 dark:text-gray-400">estemated price.</p>
+                        <h3 className="text-4xl font-bold">from 30$</h3>
+                        <p className="text-gray-500 dark:text-gray-400">*price is not fixed</p>
                       </div>
                       <ul className="space-y-2 text-sm">
-                        <li>
+                      <li>
                           <CheckIcon className="mr-2 inline-block h-4 w-4 text-green-500" />
-                          Note
+                          from 4k to 8k texture resolution
                         </li>
                         <li>
                           <CheckIcon className="mr-2 inline-block h-4 w-4 text-green-500" />
-                          Note
+                          Custom materials included
                         </li>
                         <li>
                           <CheckIcon className="mr-2 inline-block h-4 w-4 text-green-500" />
-                          Note
+                          Complicated patterns
                         </li>
                         <li>
                           <CheckIcon className="mr-2 inline-block h-4 w-4 text-green-500" />
-                          Note
+                          Complicated emssions
                         </li>
                       </ul>
                     </CardContent>
@@ -195,36 +192,47 @@ export default function Home() {
                       {/* <Button>Get Started</Button> */}
                     </CardFooter>
                   </Card>
+
+                {/* Simple cards group end */}
+
                 </div>
+              
               </TabsContent>
-              <TabsContent value="complicated">
+              <TabsContent value="avatars">
                 <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-2">
+                  
+                  {/* avatars cards group begin */}
+                  
                   <Card className="h-full">
                     <CardHeader>
-                      <CardTitle>Avatar name</CardTitle>
-                      <CardDescription>A simple avatar, with default materials, created for personal use.</CardDescription>
+                      <CardTitle>Simple avatar</CardTitle>
+                      <CardDescription>A simple avatar, with default materials, created for personal use only.</CardDescription>
                     </CardHeader>
                     <CardContent className="space-y-4">
                       <div className="space-y-1">
-                        <h3 className="text-4xl font-bold">from $1</h3>
-                        <p className="text-gray-500 dark:text-gray-400">estemated price.</p>
+                        <h3 className="text-4xl font-bold">from $40</h3>
+                        <p className="text-gray-500 dark:text-gray-400">*price is not fixed</p>
                       </div>
                       <ul className="space-y-2 text-sm">
                         <li>
                           <CheckIcon className="mr-2 inline-block h-4 w-4 text-green-500" />
-                          Note
+                          Default materials 
                         </li>
                         <li>
                           <CheckIcon className="mr-2 inline-block h-4 w-4 text-green-500" />
-                          Note
+                          Simple texture set included
                         </li>
                         <li>
                           <CheckIcon className="mr-2 inline-block h-4 w-4 text-green-500" />
-                          Note
+                          Up to 4 custom clothers included
                         </li>
                         <li>
                           <CheckIcon className="mr-2 inline-block h-4 w-4 text-green-500" />
-                          Note
+                          Up to 2 prefabs included
+                        </li>
+                        <li>
+                          <CheckIcon className="mr-2 inline-block h-4 w-4 text-green-500" />
+                          Any avatar base (Rex recomended)
                         </li>
                       </ul>
                     </CardContent>
@@ -234,30 +242,34 @@ export default function Home() {
                   </Card>
                   <Card className="h-full">
                     <CardHeader>
-                      <CardTitle>Avatar name</CardTitle>
-                      <CardDescription>A simple avatar, with default materials, created for personal use.</CardDescription>
+                      <CardTitle>Complicated avatar</CardTitle>
+                      <CardDescription>A complicated avatar, with custom materials/textures, created for personal use only.</CardDescription>
                     </CardHeader>
                     <CardContent className="space-y-4">
                       <div className="space-y-1">
-                        <h3 className="text-4xl font-bold">from $2</h3>
-                        <p className="text-gray-500 dark:text-gray-400">estemated price.</p>
+                        <h3 className="text-4xl font-bold">from $70</h3>
+                        <p className="text-gray-500 dark:text-gray-400">*price is not fixed</p>
                       </div>
                       <ul className="space-y-2 text-sm">
                         <li>
                           <CheckIcon className="mr-2 inline-block h-4 w-4 text-green-500" />
-                          Note
+                          Custom materials included
                         </li>
                         <li>
                           <CheckIcon className="mr-2 inline-block h-4 w-4 text-green-500" />
-                          Note
+                          Complicated texture sets
                         </li>
                         <li>
                           <CheckIcon className="mr-2 inline-block h-4 w-4 text-green-500" />
-                          Note
+                          Up to 8 custom clothers included
                         </li>
                         <li>
                           <CheckIcon className="mr-2 inline-block h-4 w-4 text-green-500" />
-                          Note
+                          Up to 3 prefab (custom included)
+                        </li>
+                        <li>
+                          <CheckIcon className="mr-2 inline-block h-4 w-4 text-green-500" />
+                          Any avatar base (Rex recomended)
                         </li>
                       </ul>
                     </CardContent>
@@ -265,10 +277,16 @@ export default function Home() {
                       {/* <Button>Get Started</Button> */}
                     </CardFooter>
                   </Card>
+
+                {/* avatars cards group end */}
+
                 </div>
               </TabsContent>
             </Tabs>
           </div>
+          <p className="text-center mx-auto max-w-[650px] text-gray-500 text-s/relaxed dark:text-gray-500">
+            *All terms and conditions of the order are discussed at the beginning, edits in the process are not desirable and may be rejected
+          </p>
         </div>
       </section>
       <section className="w-full py-12 md:py-24 lg:py-32">
@@ -285,15 +303,15 @@ export default function Home() {
               <div className="flex flex-col gap-2 min-[400px]:flex-row">
                 <Link
                   className="inline-flex h-10 items-center justify-center rounded-md bg-gray-900 px-8 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-gray-900/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300"
-                  href="https://google.com">Join Telegram
+                  href="https://t.me/SnejOkWinter">Join Telegram
                 </Link>
                 <Link
                   className="inline-flex h-10 items-center justify-center rounded-md bg-gray-900 px-8 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-gray-900/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300"
-                  href="https://google.com">Join Discord
+                  href="https://discord.gg/RmtrE5HkKE">Join Discord
                 </Link>
                 <Link
                   className="inline-flex h-10 items-center justify-center rounded-md bg-gray-900 px-8 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-gray-900/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300"
-                  href="https://google.com">Join VK
+                  href="https://vk.com/snowfur101">Join VK
                 </Link>
               </div>
             </div>
