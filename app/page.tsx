8b71351a61ee6d8ebcce978ad84105c72af4bb4d
Lowly <87949654+L0w1y@users.noreply.github.com>
2024-05-24 00:05:37 +0700

    rerwwqrqr

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,18 @@
+'use client'
+
 import Image from "next/image";
 import Link from "next/link"
 import { TabsTrigger, TabsList, TabsContent, Tabs } from "@/components/ui/tabs"
 import { CardTitle, CardDescription, CardHeader, CardContent, CardFooter, Card } from "@/components/ui/card"
-import { Button } from "@/components/ui/button"
+// import { Button } from "@/components/ui/button"
 // import { CheckIcon } from "lucide-react"
 import { JSX, SVGProps } from "react"
 // import { url } from "inspector";
-import Cover from "public/cover.png"; 
-import Avatar1 from "public/artworks/wm7qcP9qG1Q.jpg"; 
-import Avatar2 from "public/artworks/LM7F0uMdEU8.jpg";
-import Avatar3 from "public/artworks/MLA3TcFokaM.jpg";
-import Avatar4 from "public/artworks/MLAPTcFokaM.jpg"; 
+// import Cover from "public/cover.png"; 
+// import Avatar1 from "public/artworks/wm7qcP9qG1Q.jpg"; 
+// import Avatar2 from "public/artworks/LM7F0uMdEU8.jpg";
+// import Avatar3 from "public/artworks/MLA3TcFokaM.jpg";
+// import Avatar4 from "public/artworks/MLAPTcFokaM.jpg"; 
 // import Placeholder from "/public/placeholder.svg";
 
 export default function Home() {
@@ -20,7 +22,7 @@ export default function Home() {
         <div className="absolute inset-0">
           <Image
             alt="Hero"
-            src={Cover}
+            src="/cover.png"
             layout="fill"
             objectFit="cover"
           />
@@ -63,7 +65,7 @@ export default function Home() {
                   alt="Avatar 1"
                   className="mx-auto aspect-square overflow-hidden rounded-lg object-cover"
                   height="300"
-                  src={Avatar1}
+                  src="/artworks/wm7qcP9qG1Q.jpg"
                   width="300"
                   />
               </div>
@@ -72,7 +74,7 @@ export default function Home() {
                   alt="Avatar 2"
                   className="mx-auto aspect-square overflow-hidden rounded-lg object-cover"
                   height="300"
-                  src={Avatar2}
+                  src="/artworks/LM7F0uMdEU8.jpg"
                   width="300"
                 />
               </div>
@@ -81,7 +83,7 @@ export default function Home() {
                   alt="Avatar 3"
                   className="mx-auto aspect-square overflow-hidden rounded-lg object-cover"
                   height="300"
-                  src={Avatar3}
+                  src="/artworks/MLA3TcFokaM.jpg"
                   width="300"
                 />
               </div>
@@ -90,7 +92,7 @@ export default function Home() {
                   alt="Avatar 4"
                   className="mx-auto aspect-square overflow-hidden rounded-lg object-cover"
                   height="300"
-                  src={Avatar4}
+                  src="/artworks/MLAPTcFokaM.jpg"
                   width="300"
                 />
                 {/* <div className="mt-2 text-center">
